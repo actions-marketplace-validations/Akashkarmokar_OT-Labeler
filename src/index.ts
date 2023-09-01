6273ed0dc98b39632e07a41a0a3e9d0fa3a19779
akash <akashcsemu@gmail.com>
2023-09-01 14:34:49 +0600

    feat: printwidth changed

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,81 @@
-import { getInput, setFailed } from "@actions/core";
-import { context, getOctokit } from "@actions/github";
+import {
+  getInput,
+  setFailed,
+} from '@actions/core';
+import {
+  context,
+  getOctokit,
+} from '@actions/github';
 
-export async function run (){
-    const token = getInput('gh-token')
-    const label = getInput('label')
+export async function run() {
+  const token =
+    getInput(
+      'gh-token'
+    );
+  const label =
+    getInput(
+      'label'
+    );
 
-    const octokit = getOctokit(token);
-    const pullRequest = context.payload.pull_request;
+  const octokit =
+    getOctokit(
+      token
+    );
+  const pullRequest =
+    context.payload
+      .pull_request;
 
-    try {
-        if (!pullRequest){
-            throw new Error('This action can only be run on Pull Requests')
-        }
-        console.log({ context })
-        console.log("Owner Info: ", context.repo.owner);
-        console.log("Repository Info: ", context.repo.repo);
-
-        await octokit.rest.issues.addLabels({
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            issue_number: pullRequest.number,
-            labels: [label],
-        })
-    }catch (error) {
-        setFailed((error as Error)?.message ?? 'Unknown error')
+  try {
+    if (
+      !pullRequest
+    ) {
+      throw new Error(
+        'This action can only be run on Pull Requests'
+      );
     }
+    console.log({
+      context,
+    });
+    console.log(
+      'Owner Info: ',
+      context.repo
+        .owner
+    );
+    console.log(
+      'Repository Info: ',
+      context.repo
+        .repo
+    );
+
+    await octokit.rest.issues.addLabels(
+      {
+        owner:
+          context
+            .repo
+            .owner,
+        repo: context
+          .repo
+          .repo,
+        issue_number:
+          pullRequest.number,
+        labels: [
+          label,
+        ],
+      }
+    );
+  } catch (error) {
+    setFailed(
+      (
+        error as Error
+      )?.message ??
+        'Unknown error'
+    );
+  }
 }
 
-if (!process.env.JEST_WORKER_ID){
-    run ()
+if (
+  !process.env
+    .JEST_WORKER_ID
+) {
+  run();
 }
